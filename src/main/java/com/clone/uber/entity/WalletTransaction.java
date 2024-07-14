8193165e06e658e2a98c6e704e4fd9db8752a1c0
RonakKhuntia <khuntiarounak@gmail.com>
2024-07-14 21:24:55 +0530

    added wallet transaction entity

---
+++ src/main/java/com/clone/uber/entity/WalletTransaction.java
@@ -0,0 +1,26 @@
+package com.clone.uber.entity;
+
+import com.clone.uber.entity.enums.TransactionMethod;
+import com.clone.uber.entity.enums.TransactionType;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class WalletTransaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Double amount;
+    private TransactionType transactionType;
+    private TransactionMethod transactionMethod;
+    @OneToOne
+    private Ride ride;
+    private String transactionId;
+    @ManyToOne
+    private Wallet wallet;
+    @CreationTimestamp
+    private LocalDateTime timeStamp;
+}
