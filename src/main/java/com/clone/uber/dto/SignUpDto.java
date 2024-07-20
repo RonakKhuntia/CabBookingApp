bc04165810d91cd3cd20732613461612da76f9b6
RonakKhuntia <khuntiarounak@gmail.com>
2024-07-20 19:53:53 +0530

    added dto classes

---
+++ src/main/java/com/clone/uber/dto/SignUpDto.java
@@ -0,0 +1,15 @@
+package com.clone.uber.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignUpDto {
+
+    private String name;
+    private String email;
+    private String password;
+}
