319ca8376bffe73f9e16f382fd23cb853cd7d1dd
RonakKhuntia <khuntiarounak@gmail.com>
2024-07-20 19:54:24 +0530

    added repository classes

---
+++ src/main/java/com/clone/uber/repository/DriverRepository.java
@@ -0,0 +1,9 @@
+package com.clone.uber.repository;
+
+import com.clone.uber.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Long> {
+}
---
+++ src/main/java/com/clone/uber/repository/RideRepository.java
@@ -0,0 +1,9 @@
+package com.clone.uber.repository;
+
+import com.clone.uber.entity.Ride;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RideRepository extends JpaRepository<Ride, Long> {
+}
