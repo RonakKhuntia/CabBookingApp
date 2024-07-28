a44dc640b74368ece7fc08cdf9e66fce81e3b639
RonakKhuntia <khuntiarounak@gmail.com>
2024-07-28 15:01:24 +0530

    add create ride impl

--- src/main/java/com/clone/uber/service/impl/RideServiceImpl.java
+++ src/main/java/com/clone/uber/service/impl/RideServiceImpl.java
@@ -3,17 +3,32 @@ package com.clone.uber.service.impl;
 import com.clone.uber.dto.RideRequestDto;
 import com.clone.uber.entity.Driver;
 import com.clone.uber.entity.Ride;
+import com.clone.uber.entity.RideRequest;
+import com.clone.uber.entity.enums.RideRequestStatus;
 import com.clone.uber.entity.enums.RideStatus;
+import com.clone.uber.exception.ResourceNotFoundException;
+import com.clone.uber.repository.RideRepository;
+import com.clone.uber.service.RideRequestService;
 import com.clone.uber.service.RideService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
+@RequiredArgsConstructor
 public class RideServiceImpl implements RideService {
+    private final RideRepository rideRepository;
+    private final RideRequestService rideRequestService;
+    private final ModelMapper modelMapper;
+
     @Override
     public Ride getRideById(Long rideId) {
-        return null;
+        return rideRepository.findById(rideId)
+                .orElseThrow(() -> new ResourceNotFoundException("Ride not found with id: "+rideId));
     }
 
     @Override
@@ -22,13 +37,24 @@ public class RideServiceImpl implements RideService {
     }
 
     @Override
-    public Ride createNewRide(RideRequestDto rideRequestDto, Driver driver) {
-        return null;
+    public Ride createNewRide(RideRequest rideRequest, Driver driver) {
+        rideRequest.setRideRequestStatus(RideRequestStatus.CONFIRMED);
+
+        Ride ride = modelMapper.map(rideRequest, Ride.class);
+        ride.setRideRequestStatus(RideStatus.CONFIRMED);
+        ride.setDriver(driver);
+        ride.setOtp(generateRandomOTP());
+        ride.setId(null);
+
+        rideRequestService.update(rideRequest);
+        return rideRepository.save(ride);
     }
 
+
     @Override
-    public Ride updateRideStatus(Long rideId, RideStatus rideStatus) {
-        return null;
+    public Ride updateRideStatus(Ride ride, RideStatus rideStatus) {
+        ride.setRideRequestStatus(rideStatus);
+        return rideRepository.save(ride);
     }
 
     @Override
@@ -37,7 +63,14 @@ public class RideServiceImpl implements RideService {
     }
 
     @Override
-    public Page<Ride> getAllRidesofDriver(Long driverId, PageRequest pageRequest) {
+    public Page<Ride> getAllRidesOfDriver(Long driverId, PageRequest pageRequest) {
         return null;
+    }
+
+
+    private String generateRandomOTP() {
+        Random random = new Random();
+        int otpInt = random.nextInt(10000);  //0 to 9999
+        return String.format("%04d", otpInt);
     }
 }
