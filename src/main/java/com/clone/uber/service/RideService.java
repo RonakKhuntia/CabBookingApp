cc640d587a9774cdcfa2e95595d06a1e530d42dc
RonakKhuntia <khuntiarounak@gmail.com>
2024-07-28 15:00:10 +0530

    changed dto params to entity in ride service

--- src/main/java/com/clone/uber/service/RideService.java
+++ src/main/java/com/clone/uber/service/RideService.java
@@ -3,6 +3,7 @@ package com.clone.uber.service;
 import com.clone.uber.dto.RideRequestDto;
 import com.clone.uber.entity.Driver;
 import com.clone.uber.entity.Ride;
+import com.clone.uber.entity.RideRequest;
 import com.clone.uber.entity.enums.RideStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,9 +11,14 @@ import org.springframework.data.domain.PageRequest;
 public interface RideService {
 
     Ride getRideById(Long rideId);
+
     void matchWithDrivers(RideRequestDto rideRequestDto);
-    Ride createNewRide(RideRequestDto rideRequestDto, Driver driver);
-    Ride updateRideStatus(Long rideId, RideStatus rideStatus);
+
+    Ride createNewRide(RideRequest rideRequest, Driver driver);
+
+    Ride updateRideStatus(Ride ride, RideStatus rideStatus);
+
     Page<Ride> getAllRidesOfRider(Long riderId, PageRequest pageRequest);
-    Page<Ride> getAllRidesofDriver(Long driverId, PageRequest pageRequest);
+
+    Page<Ride> getAllRidesOfDriver(Long driverId, PageRequest pageRequest);
 }
