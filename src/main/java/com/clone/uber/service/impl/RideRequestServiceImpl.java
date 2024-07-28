a1f7389b691e59f22c438b05eba49153beb44d35
RonakKhuntia <khuntiarounak@gmail.com>
2024-07-28 14:34:46 +0530

    add ride request service impl

---
+++ src/main/java/com/clone/uber/service/impl/RideRequestServiceImpl.java
@@ -0,0 +1,28 @@
+package com.clone.uber.service.impl;
+
+import com.clone.uber.entity.RideRequest;
+import com.clone.uber.exception.ResourceNotFoundException;
+import com.clone.uber.repository.RideRequestRepository;
+import com.clone.uber.service.RideRequestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RideRequestServiceImpl implements RideRequestService {
+
+    private final RideRequestRepository rideRequestRepository;
+
+    @Override
+    public RideRequest findRideRequestById(Long rideRequestId) {
+        return rideRequestRepository.findById(rideRequestId)
+                .orElseThrow(() -> new ResourceNotFoundException("RideRequest not found with id: "+rideRequestId));
+    }
+
+    @Override
+    public void update(RideRequest rideRequest) {
+        rideRequestRepository.findById(rideRequest.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("RideRequest not found with id: "+rideRequest.getId()));
+        rideRequestRepository.save(rideRequest);
+    }
+}
--- src/main/java/com/clone/uber/service/impl/RiderServiceImpl.java
+++ src/main/java/com/clone/uber/service/impl/RiderServiceImpl.java
@@ -4,15 +4,18 @@ import com.clone.uber.dto.DriverDto;
 import com.clone.uber.dto.RideDto;
 import com.clone.uber.dto.RideRequestDto;
 import com.clone.uber.dto.RiderDto;
+import com.clone.uber.entity.Driver;
 import com.clone.uber.entity.RideRequest;
 import com.clone.uber.entity.Rider;
 import com.clone.uber.entity.User;
 import com.clone.uber.entity.enums.RideRequestStatus;
+import com.clone.uber.exception.ResourceNotFoundException;
 import com.clone.uber.repository.RideRequestRepository;
 import com.clone.uber.repository.RiderRepository;
 import com.clone.uber.service.RiderService;
 import com.clone.uber.strategy.DriverMatchingStrategy;
 import com.clone.uber.strategy.RideFareCalculationStrategy;
+import com.clone.uber.strategy.RideStrategyManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -26,21 +29,38 @@ import java.util.List;
 public class RiderServiceImpl implements RiderService {
 
     private final ModelMapper modelMapper;
-    private final RideFareCalculationStrategy rideFareCalculationStrategy;
-    private final DriverMatchingStrategy driverMatchingStrategy;
+//    private final RideFareCalculationStrategy rideFareCalculationStrategy;
+//    private final DriverMatchingStrategy driverMatchingStrategy;
     private final RideRequestRepository rideRequestRepository;
     private final RiderRepository riderRepository;
+    private final RideStrategyManager rideStrategyManager;
 
     //request ride service impl
     @Override
     public RideRequestDto requestRide(RideRequestDto rideRequestDto) {
-        RideRequest rideRequest = modelMapper.map(rideRequestDto,RideRequest.class);
+        Rider rider = getCurrentRider();
+        RideRequest rideRequest = modelMapper.map(rideRequestDto, RideRequest.class);
         rideRequest.setRideRequestStatus(RideRequestStatus.PENDING);
-        Double fare = rideFareCalculationStrategy.calculateFare(rideRequest);
+        rideRequest.setRider(rider);
+
+        Double fare = rideStrategyManager.rideFareCalculationStrategy().calculateFare(rideRequest);
         rideRequest.setFare(fare);
+
         RideRequest savedRideRequest = rideRequestRepository.save(rideRequest);
-        driverMatchingStrategy.findMatchingDriver(rideRequest);
-        return modelMapper.map(savedRideRequest,RideRequestDto.class);
+
+        List<Driver> drivers = rideStrategyManager
+                .driverMatchingStrategy(rider.getRating()).findMatchingDriver(rideRequest);
+
+//        TODO : Send notification to all the drivers about this ride request
+
+        return modelMapper.map(savedRideRequest, RideRequestDto.class);
+    }
+
+    private Rider getCurrentRider() {
+        //TODO : implement Spring security
+        return riderRepository.findById(1L).orElseThrow(() -> new ResourceNotFoundException(
+                "Rider not found with id: "+1
+        ));
     }
 
     @Override
