bb2f4e86cff27ab6d94d5a0e94775f26cd0196b0
RonakKhuntia <khuntiarounak@gmail.com>
2024-07-28 14:59:36 +0530

    add otp param to driver startride method

--- src/main/java/com/clone/uber/service/DriverService.java
+++ src/main/java/com/clone/uber/service/DriverService.java
@@ -3,6 +3,7 @@ package com.clone.uber.service;
 import com.clone.uber.dto.DriverDto;
 import com.clone.uber.dto.RideDto;
 import com.clone.uber.dto.RiderDto;
+import com.clone.uber.entity.Driver;
 
 import java.util.List;
 
@@ -10,9 +11,10 @@ public interface DriverService {
 
     RideDto acceptRide(Long rideId);
     RideDto cancelRide(Long rideId);
-    RideDto startRide(Long rideId);
+    RideDto startRide(Long rideId, String otp);
     RideDto endRide(Long rideId);
     RiderDto rateRider(Long rideId,Integer rating);
     DriverDto getMyProfile();
     List<RideDto> getAllMyRides();
+    Driver getCurrentDriver();
 }
