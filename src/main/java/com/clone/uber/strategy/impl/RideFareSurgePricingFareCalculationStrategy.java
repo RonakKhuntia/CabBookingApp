09eb3d856f650ed689880ed67f64e6e76c2c7cd4
RonakKhuntia <khuntiarounak@gmail.com>
2024-07-28 14:33:10 +0530

    implemented drivermatching and faresurge strategy

--- src/main/java/com/clone/uber/strategy/impl/RideFareSurgePricingFareCalculationStrategy.java
+++ src/main/java/com/clone/uber/strategy/impl/RideFareSurgePricingFareCalculationStrategy.java
@@ -2,15 +2,22 @@ package com.clone.uber.strategy.impl;
 
 import com.clone.uber.dto.RideRequestDto;
 import com.clone.uber.entity.RideRequest;
+import com.clone.uber.service.DistanceService;
 import com.clone.uber.strategy.RideFareCalculationStrategy;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RideFareSurgePricingFareCalculationStrategy implements RideFareCalculationStrategy {
 
-    //ride fare surge strategy impl
+    private final DistanceService distanceService;
+    private static final double SURGE_FACTOR = 2;
+
     @Override
     public double calculateFare(RideRequest rideRequest) {
-        return 0;
+        double distance = distanceService.calculateDistance(rideRequest.getPickupLocation(),
+                rideRequest.getDropOffLocation());
+        return distance*RIDE_FARE_MULTIPLIER*SURGE_FACTOR;
     }
 }
