ffed01502a5bf44c91f995234c0c8d0807fc8265
RonakKhuntia <khuntiarounak@gmail.com>
2024-08-08 11:43:06 +0530

    change List to Page in getAllRides in RiderService

--- src/main/java/com/clone/uber/service/RiderService.java
+++ src/main/java/com/clone/uber/service/RiderService.java
@@ -7,16 +7,23 @@ import com.clone.uber.dto.RiderDto;
 import com.clone.uber.entity.RideRequest;
 import com.clone.uber.entity.Rider;
 import com.clone.uber.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
 public interface RiderService {
     RideRequestDto requestRide(RideRequestDto rideRequestDto);
-    RideDto cancelRide(Long rideId);
-    DriverDto rateDriver(Long rideId, Integer rating);
-    RiderDto getMyProfile();
-    List<RideDto> getAllMyRides();
 
-    //create new rider
+    RideDto cancelRide(Long rideId);
+
+    DriverDto rateDriver(Long rideId, Integer rating);
+
+    RiderDto getMyProfile();
+
+    Page<RideDto> getAllMyRides(PageRequest pageRequest);
+
     Rider createNewRider(User user);
+
+    Rider getCurrentRider();
 }
