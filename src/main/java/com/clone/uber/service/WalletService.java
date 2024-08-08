2c66b6531a4baff6fcac379e3a86f0fb9fce19ac
RonakKhuntia <khuntiarounak@gmail.com>
2024-08-08 11:44:22 +0530

    add wallet and transaction service and implementation

---
+++ src/main/java/com/clone/uber/service/WalletService.java
@@ -0,0 +1,26 @@
+package com.clone.uber.service;
+
+import com.clone.uber.entity.Ride;
+import com.clone.uber.entity.User;
+import com.clone.uber.entity.Wallet;
+import com.clone.uber.entity.enums.TransactionMethod;
+
+public interface WalletService {
+
+    Wallet addMoneyToWallet(User user, Double amount,
+                            String transactionId, Ride ride,
+                            TransactionMethod transactionMethod);
+
+    Wallet deductMoneyFromWallet(User user, Double amount,
+                                 String transactionId, Ride ride,
+                                 TransactionMethod transactionMethod);
+
+    void withdrawAllMyMoneyFromWallet();
+
+    Wallet findWalletById(Long walletId);
+
+    Wallet createNewWallet(User user);
+
+    Wallet findByUser(User user);
+
+}
---
+++ src/main/java/com/clone/uber/service/WalletTransactionService.java
@@ -0,0 +1,9 @@
+package com.clone.uber.service;
+
+import com.clone.uber.entity.WalletTransaction;
+
+public interface WalletTransactionService {
+
+    void createNewWalletTransaction(WalletTransaction walletTransaction);
+
+}
