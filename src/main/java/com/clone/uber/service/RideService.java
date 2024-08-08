8d49b586cd1c7439c64132ccbc65c7efff1c9844
RonakKhuntia <khuntiarounak@gmail.com>
2024-08-08 11:43:37 +0530

    change getAllRides param from id to entity

--- src/main/java/com/clone/uber/service/RideService.java
+++ src/main/java/com/clone/uber/service/RideService.java
@@ -4,6 +4,7 @@ import com.clone.uber.dto.RideRequestDto;
 import com.clone.uber.entity.Driver;
 import com.clone.uber.entity.Ride;
 import com.clone.uber.entity.RideRequest;
+import com.clone.uber.entity.Rider;
 import com.clone.uber.entity.enums.RideStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,13 +13,11 @@ public interface RideService {
 
     Ride getRideById(Long rideId);
 
-    void matchWithDrivers(RideRequestDto rideRequestDto);
-
     Ride createNewRide(RideRequest rideRequest, Driver driver);
 
     Ride updateRideStatus(Ride ride, RideStatus rideStatus);
 
-    Page<Ride> getAllRidesOfRider(Long riderId, PageRequest pageRequest);
+    Page<Ride> getAllRidesOfRider(Rider rider, PageRequest pageRequest);
 
-    Page<Ride> getAllRidesOfDriver(Long driverId, PageRequest pageRequest);
+    Page<Ride> getAllRidesOfDriver(Driver driver, PageRequest pageRequest);
 }
