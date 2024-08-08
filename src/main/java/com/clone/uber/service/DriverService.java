db434a1e8481f25447760f21f9e8270a889f63e9
RonakKhuntia <khuntiarounak@gmail.com>
2024-08-08 11:41:34 +0530

    change return type from List to Page for getAllRides method in DriverService

--- src/main/java/com/clone/uber/service/DriverService.java
+++ src/main/java/com/clone/uber/service/DriverService.java
@@ -4,17 +4,28 @@ import com.clone.uber.dto.DriverDto;
 import com.clone.uber.dto.RideDto;
 import com.clone.uber.dto.RiderDto;
 import com.clone.uber.entity.Driver;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
 public interface DriverService {
 
-    RideDto acceptRide(Long rideId);
+    RideDto acceptRide(Long rideRequestId);
+
     RideDto cancelRide(Long rideId);
+
     RideDto startRide(Long rideId, String otp);
+
     RideDto endRide(Long rideId);
-    RiderDto rateRider(Long rideId,Integer rating);
+
+    RiderDto rateRider(Long rideId, Integer rating);
+
     DriverDto getMyProfile();
-    List<RideDto> getAllMyRides();
+
+    Page<RideDto> getAllMyRides(PageRequest pageRequest);
+
     Driver getCurrentDriver();
+
+    Driver updateDriverAvailability(Driver driver, boolean available);
 }
