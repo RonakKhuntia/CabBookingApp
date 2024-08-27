e3162a833e2a8cbfdeec138455a3b9f471c71e2e
RonakKhuntia <khuntiarounak@gmail.com>
2024-08-27 20:45:13 +0530

    added onboardnewdriver impl

--- src/main/java/com/clone/uber/controller/AuthController.java
+++ src/main/java/com/clone/uber/controller/AuthController.java
@@ -1,13 +1,14 @@
 package com.clone.uber.controller;
 
+import com.clone.uber.dto.DriverDto;
 import com.clone.uber.dto.SignUpDto;
 import com.clone.uber.dto.UserDto;
 import com.clone.uber.service.AuthService;
+import com.clone.uber.dto.OnboardDriverDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,6 +20,12 @@ public class AuthController {
     @PostMapping("/signUp")
     public UserDto signUp(@RequestBody SignUpDto signUpDto){
         return authService.signup(signUpDto);
+    }
+
+    @PostMapping("/onBoardNewDriver/{userId}")
+    ResponseEntity<DriverDto> onBoardNewDriver(@PathVariable Long userId, @RequestBody OnboardDriverDto onboardDriverDto) {
+        return new ResponseEntity<>(authService.onboardNewDriver(userId,
+                onboardDriverDto.getVehicleId()), HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/clone/uber/service/AuthService.java
+++ src/main/java/com/clone/uber/service/AuthService.java
@@ -8,5 +8,5 @@ public interface AuthService {
 
     String login(String email, String password);
     UserDto signup(SignUpDto signUpDto);
-    DriverDto onboardNewDriver(Long userId);
+    DriverDto onboardNewDriver(Long userId, String vehicleId);
 }
