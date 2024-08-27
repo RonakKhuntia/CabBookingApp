a8b1f1404061b8ccf92e19da56f402eeea4dd45c
RonakKhuntia <khuntiarounak@gmail.com>
2024-08-27 20:47:14 +0530

    added signup and onboardnewdriver impl in auth controller

--- src/main/java/com/clone/uber/service/impl/AuthServiceImpl.java
+++ src/main/java/com/clone/uber/service/impl/AuthServiceImpl.java
@@ -3,18 +3,23 @@ package com.clone.uber.service.impl;
 import com.clone.uber.dto.DriverDto;
 import com.clone.uber.dto.SignUpDto;
 import com.clone.uber.dto.UserDto;
+import com.clone.uber.entity.Driver;
 import com.clone.uber.entity.User;
 import com.clone.uber.entity.enums.Role;
+import com.clone.uber.exception.ResourceNotFoundException;
 import com.clone.uber.exception.RuntimeConflictException;
 import com.clone.uber.repository.UserRepository;
 import com.clone.uber.service.AuthService;
+import com.clone.uber.service.DriverService;
 import com.clone.uber.service.RiderService;
+import com.clone.uber.service.WalletService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
 import java.util.Set;
+
+import static com.clone.uber.entity.enums.Role.DRIVER;
 
 @Service
 @RequiredArgsConstructor
@@ -23,10 +28,12 @@ public class AuthServiceImpl implements AuthService {
     private final ModelMapper modelMapper;
     private final UserRepository userRepository;
     private final RiderService riderService;
+    private final DriverService driverService;
+    private final WalletService walletService;
 
     @Override
     public String login(String email, String password) {
-        return null;
+        return "";
     }
 
     //sign up service impl
@@ -34,22 +41,38 @@ public class AuthServiceImpl implements AuthService {
     @Transactional
     public UserDto signup(SignUpDto signUpDto) {
         User user = userRepository.findByEmail(signUpDto.getEmail()).orElse(null);
-        if(user == null){
-            throw new RuntimeConflictException("User already exists with email : "+signUpDto.getEmail());
-        }
-        User user1 = modelMapper.map(signUpDto, User.class);
-        user1.setRoles(Set.of(Role.RIDER));
-        User savedUser = userRepository.save(user1);
+        if(user != null)
+            throw new RuntimeConflictException("Cannot signup, User already exists with email "+signUpDto.getEmail());
+
+        User mappedUser = modelMapper.map(signUpDto, User.class);
+        mappedUser.setRoles(Set.of(Role.RIDER));
+        User savedUser = userRepository.save(mappedUser);
+
+//        create user related entities
         riderService.createNewRider(savedUser);
+        walletService.createNewWallet(savedUser);
 
-        //TODO Add Wallet Service
-
-        return modelMapper.map(savedUser,UserDto.class);
+        return modelMapper.map(savedUser, UserDto.class);
     }
 
     @Override
-    public DriverDto onboardNewDriver(Long userId) {
-        return null;
+    public DriverDto onboardNewDriver(Long userId, String vehicleId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException(STR."User not found with id \{userId}"));
+
+        if(user.getRoles().contains(DRIVER))
+            throw new RuntimeConflictException(STR."User with id \{userId} is already a Driver");
+
+        Driver createDriver = Driver.builder()
+                .user(user)
+                .rating(0.0)
+                .vehicleId(vehicleId)
+                .available(true)
+                .build();
+        user.getRoles().add(DRIVER);
+        userRepository.save(user);
+        Driver savedDriver = driverService.createNewDriver(createDriver);
+        return modelMapper.map(savedDriver, DriverDto.class);
     }
 
 }
--- src/main/java/com/clone/uber/service/impl/WalletServiceImpl.java
+++ src/main/java/com/clone/uber/service/impl/WalletServiceImpl.java
@@ -60,9 +60,9 @@ public class WalletServiceImpl implements WalletService {
                 .amount(amount)
                 .build();
 
-        walletTransactionService.createNewWalletTransaction(walletTransaction);
+        //walletTransactionService.createNewWalletTransaction(walletTransaction);
 
-//        wallet.getTransactions().add(walletTransaction);
+        wallet.getTransactions().add(walletTransaction);
 
         return walletRepository.save(wallet);
     }
