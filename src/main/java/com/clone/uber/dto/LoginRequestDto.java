196204a2b3fc8a1b1f3996e040fa89a50db799ef
RonakKhuntia <khuntiarounak@gmail.com>
2024-08-29 22:26:47 +0530

    added login request response dto

---
+++ src/main/java/com/clone/uber/dto/LoginRequestDto.java
@@ -0,0 +1,9 @@
+package com.clone.uber.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequestDto {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/clone/uber/dto/LoginResponseDto.java
@@ -0,0 +1,12 @@
+package com.clone.uber.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDto {
+    private String accessToken;
+}
