a7675f032355a73442b471c335b8ac7a52a22cae
RonakKhuntia <khuntiarounak@gmail.com>
2024-08-29 22:41:52 +0530

    added findByUser method for DriverRepository

--- src/main/java/com/clone/uber/repository/DriverRepository.java
+++ src/main/java/com/clone/uber/repository/DriverRepository.java
@@ -1,34 +1,33 @@
 package com.clone.uber.repository;
 
 import com.clone.uber.entity.Driver;
+import com.clone.uber.entity.User;
 import org.locationtech.jts.geom.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 //ST_Distance(point1,point2)
 //ST_DWithin(point1, 10000)
 
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
-
-    //find ten nearest drivers query
     @Query(value = "SELECT d.*, ST_Distance(d.current_location, :pickupLocation) AS distance " +
-            "FROM drives d " +
+            "FROM driver d " +
             "WHERE d.available = true AND ST_DWithin(d.current_location, :pickupLocation, 10000) " +
             "ORDER BY distance " +
-            "LIMIT 10 ",
-            nativeQuery = true
-    )
+            "LIMIT 10", nativeQuery = true)
     List<Driver> findTenNearestDrivers(Point pickupLocation);
 
     @Query(value = "SELECT d.* " +
-            "FROM drivers d " +
+            "FROM driver d " +
             "WHERE d.available = true AND ST_DWithin(d.current_location, :pickupLocation, 15000) " +
             "ORDER BY d.rating DESC " +
-            "LIMIT 10", nativeQuery = true
-    )
+            "LIMIT 10", nativeQuery = true)
     List<Driver> findTenNearbyTopRatedDrivers(Point pickupLocation);
+
+    Optional<Driver> findByUser(User user);
 }
