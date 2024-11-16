1db857b8cb7423afdb3dbdc239a5bdeab4c14c3a
rounak.khuntia@maersk.com <rounak.khuntia@maersk.com>
2024-11-16 22:56:54 +0530

    added user entity and user role enum

---
+++ src/main/java/com/clone/backend/uber/entity/User.java
@@ -0,0 +1,21 @@
+package com.clone.backend.uber.entity;
+
+import com.clone.backend.uber.enums.Role;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "app_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @ElementCollection(fetch = FetchType.LAZY)
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles;
+}
---
+++ src/main/java/com/clone/backend/uber/enums/Role.java
@@ -0,0 +1,7 @@
+package com.clone.backend.uber.enums;
+
+public enum Role {
+    ADMIN,
+    DRIVER,
+    RIDER
+}
