0c9d7486476afc1c6bdd0ae8e74cb905b542da3a
rounak.khuntia@maersk.com <khuntiarounak@gmail.com>
2024-11-19 17:29:06 +0530

    Update RiderService.java

--- src/main/java/com/clone/backend/uber/service/RiderService.java
+++ src/main/java/com/clone/backend/uber/service/RiderService.java
@@ -4,6 +4,8 @@ import com.clone.backend.uber.dto.DriverDto;
 import com.clone.backend.uber.dto.RideDto;
 import com.clone.backend.uber.dto.RideRequestDto;
 import com.clone.backend.uber.dto.RiderDto;
+import com.clone.backend.uber.entity.Rider;
+import com.clone.backend.uber.entity.User;
 
 import java.util.List;
 
@@ -13,4 +15,5 @@ public interface RiderService {
     DriverDto rateDriver(Long rideId, Integer rating);
     RiderDto getMyProfile();
     List<RideDto> getMyRides();
+    Rider createNewRider(User user);
 }
