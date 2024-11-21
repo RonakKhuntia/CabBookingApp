c348231d212332c862dc23a25f43224516a22e84
rounakkhuntia <khuntiarounak@gmail.com>
2024-11-21 21:37:48 +0530

    added saveriderequesttorepository method for riderequestservice

--- src/main/java/com/clone/backend/uber/service/impl/RideRequestServiceImpl.java
+++ src/main/java/com/clone/backend/uber/service/impl/RideRequestServiceImpl.java
@@ -1,6 +1,5 @@
 package com.clone.backend.uber.service.impl;
 
-
 import com.clone.backend.uber.entity.RideRequest;
 import com.clone.backend.uber.exception.ResourceNotFoundException;
 import com.clone.backend.uber.repository.RideRequestRepository;
@@ -20,5 +19,11 @@ public class RideRequestServiceImpl implements RideRequestService {
                 .findById(rideRequestId)
                 .orElseThrow(() -> new ResourceNotFoundException(String.format("Ride Request Not Found with id %s",
                         rideRequestId)));
+    }
+
+    @Override
+    public void saveRideRequestToRepository(RideRequest rideRequest) {
+        findRideRequestById(rideRequest.getId());
+        rideRequestRepository.save(rideRequest);
     }
 }
