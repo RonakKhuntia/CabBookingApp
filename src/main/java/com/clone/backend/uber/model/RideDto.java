9f9afa1b392e06629279617e4ca2d756d320abb8
rounakkhuntia <khuntiarounak@gmail.com>
2024-11-21 21:36:49 +0530

    fixed ride dto fields model mapper issue

--- src/main/java/com/clone/backend/uber/model/RideDto.java
+++ src/main/java/com/clone/backend/uber/model/RideDto.java
@@ -3,16 +3,18 @@ package com.clone.backend.uber.model;
 import com.clone.backend.uber.entity.Rider;
 import com.clone.backend.uber.enums.PaymentMethod;
 import com.clone.backend.uber.enums.RideStatus;
+import lombok.Data;
 import org.locationtech.jts.geom.Point;
 
 import java.time.LocalDateTime;
 
+@Data
 public class RideDto {
     private Long id;
-    private Point pickupLocation;
-    private Point dropOffLocation;
+    private PointDto pickupLocation;
+    private PointDto dropOffLocation;
     private LocalDateTime createdTime;
-    private Rider rider;
+    private RiderDto rider;
     private String otp;
     private DriverDto driver;
     private PaymentMethod paymentMethod;
