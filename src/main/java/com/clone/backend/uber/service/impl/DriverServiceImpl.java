71a25a3e13a08bd6f72b98e3688fb60b69998639
rounakkhuntia <khuntiarounak@gmail.com>
2024-11-21 22:22:02 +0530

    added acceptRide and startRide methods implementation for driver service

--- src/main/java/com/clone/backend/uber/service/impl/DriverServiceImpl.java
+++ src/main/java/com/clone/backend/uber/service/impl/DriverServiceImpl.java
@@ -1,18 +1,52 @@
 package com.clone.backend.uber.service.impl;
 
+import com.clone.backend.uber.entity.Driver;
+import com.clone.backend.uber.entity.Ride;
+import com.clone.backend.uber.entity.RideRequest;
+import com.clone.backend.uber.enums.RideRequestStatus;
+import com.clone.backend.uber.enums.RideStatus;
+import com.clone.backend.uber.exception.ResourceNotFoundException;
 import com.clone.backend.uber.model.DriverDto;
 import com.clone.backend.uber.model.RideDto;
 import com.clone.backend.uber.model.RiderDto;
+import com.clone.backend.uber.repository.DriverRepository;
 import com.clone.backend.uber.service.DriverService;
+import com.clone.backend.uber.service.RideRequestService;
+import com.clone.backend.uber.service.RideService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class DriverServiceImpl implements DriverService {
+
+    private final DriverRepository driverRepository;
+    private final RideRequestService rideRequestService;
+    private final RideService rideService;
+    private final ModelMapper modelMapper;
+
     @Override
-    public RideDto acceptRide(Long rideId) {
-        return null;
+    @Transactional
+    public RideDto acceptRide(Long rideRequestId) {
+        RideRequest rideRequest = rideRequestService.findRideRequestById(rideRequestId);
+        if (!rideRequest.getRideRequestStatus().equals(RideRequestStatus.PENDING)) {
+            throw new RuntimeException(String.format("RideRequest cannot be accepted, status is %s",
+                    rideRequest.getRideRequestStatus()));
+        }
+        rideRequest.setRideRequestStatus(RideRequestStatus.CONFIRMED);
+        Driver currentDriver = getCurrentDriver();
+        if (!currentDriver.getAvailable()) {
+            throw new RuntimeException("Driver cannot accept ride due to unavailability");
+        }
+        currentDriver.setAvailable(false);
+        Driver savedDriver = driverRepository.save(currentDriver);
+        Ride ride = rideService.createNewRide(rideRequest, savedDriver);
+        return modelMapper.map(ride, RideDto.class);
     }
 
     @Override
@@ -21,8 +55,21 @@ public class DriverServiceImpl implements DriverService {
     }
 
     @Override
-    public RideDto startRide(Long rideId) {
-        return null;
+    public RideDto startRide(Long rideId, String otp) {
+        Ride ride = rideService.getRideById(rideId);
+        Driver driver = getCurrentDriver();
+        if (!driver.equals(ride.getDriver())) {
+            throw new RuntimeException("Driver cannot start a ride as he had not accepted it earlier");
+        }
+        if (!ride.getRideStatus().equals(RideStatus.CONFIRMED)) {
+            throw new RuntimeException(String.format("Ride cannot be started as status is %s", ride.getRideStatus()));
+        }
+        if (!ride.getOtp().equals(otp)) {
+            throw new RuntimeException(String.format("OTP %s is not valid", otp));
+        }
+        ride.setStartedAt(LocalDateTime.now());
+        Ride savedRide = rideService.updateRideStatus(ride, RideStatus.ONGOING);
+        return modelMapper.map(savedRide, RideDto.class);
     }
 
     @Override
@@ -44,4 +91,11 @@ public class DriverServiceImpl implements DriverService {
     public List<RideDto> getMyRides() {
         return List.of();
     }
+
+    @Override
+    public Driver getCurrentDriver() {
+        return driverRepository.findById(2L).orElseThrow(() -> new ResourceNotFoundException(
+                String.format("Driver not found with id %s", 2)));
+    }
+
 }
