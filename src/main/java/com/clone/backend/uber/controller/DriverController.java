866350e95076fd5b098fe02208f0ea01df354da7
rounakkhuntia <khuntiarounak@gmail.com>
2024-11-21 22:14:00 +0530

    added start ride method implementation for driver controller

--- src/main/java/com/clone/backend/uber/controller/DriverController.java
+++ src/main/java/com/clone/backend/uber/controller/DriverController.java
@@ -1,13 +1,11 @@
 package com.clone.backend.uber.controller;
 
 import com.clone.backend.uber.model.RideDto;
+import com.clone.backend.uber.model.RideStartDto;
 import com.clone.backend.uber.service.DriverService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/driver")
@@ -19,6 +17,12 @@ public class DriverController {
     @PostMapping("/acceptRide/{rideRequestId}")
     public ResponseEntity<RideDto> acceptRide(@PathVariable Long rideRequestId) {
         return ResponseEntity.ok(driverService.acceptRide(rideRequestId));
+    }
+
+    @PostMapping("/startRide/{rideRequestId}")
+    public ResponseEntity<RideDto> startRide(@PathVariable Long rideRequestId,
+                                             @RequestBody RideStartDto rideStartDto) {
+        return ResponseEntity.ok(driverService.startRide(rideRequestId, rideStartDto.getOtp()));
     }
 
 }
