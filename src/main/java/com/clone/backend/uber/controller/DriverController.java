c506077d6c1a8530fefdcb687449bf7fb6ebcd3e
rounakkhuntia <khuntiarounak@gmail.com>
2024-11-21 21:35:21 +0530

    added driver controller

---
+++ src/main/java/com/clone/backend/uber/controller/DriverController.java
@@ -0,0 +1,24 @@
+package com.clone.backend.uber.controller;
+
+import com.clone.backend.uber.model.RideDto;
+import com.clone.backend.uber.service.DriverService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/driver")
+@RequiredArgsConstructor
+public class DriverController {
+
+    private final DriverService driverService;
+
+    @PostMapping("/acceptRide/{rideRequestId}")
+    public ResponseEntity<RideDto> acceptRide(@PathVariable Long rideRequestId) {
+        return ResponseEntity.ok(driverService.acceptRide(rideRequestId));
+    }
+
+}
