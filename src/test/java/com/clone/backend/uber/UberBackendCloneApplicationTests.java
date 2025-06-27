26595adf782c0a14071bb4462248e1b762b2fe56
rounakkhuntia <khuntiarounak@gmail.com>
2025-06-27 14:42:56 +0530

    Update UberBackendCloneApplicationTests.java

--- src/test/java/com/clone/backend/uber/UberBackendCloneApplicationTests.java
+++ src/test/java/com/clone/backend/uber/UberBackendCloneApplicationTests.java
@@ -6,16 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UberBackendCloneApplicationTests {
-
-	@Autowired
-	EmailSenderService emailSenderService;
-
-	@Test
-	void contextLoads() {
-		String[] emails = {"cihihog208@gholar.com", "2041011036.rounakkhuntia@gmail.com"};
-		emailSenderService.sendEmail(emails,
-				"test email subject",
-				"test email body");
-	}
-
 }
